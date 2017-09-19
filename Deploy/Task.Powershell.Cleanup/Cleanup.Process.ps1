34a8fa1719313a965ee0a86c1063c1dff9978534
rahul24 <rahul.developer@gmail.com>
2017-09-19 14:48:54 +0530

    VSTS tasks config for deployment
    1. Tokenizer Task.
    2. Powershell to cleanup.

---
+++ Deploy/Task.Powershell.Cleanup/Cleanup.Process.ps1
@@ -0,0 +1,14 @@
+﻿try {	
+	#PowerShell -ExecutionPolicy Bypass -File "\\Co1bpdcsvctfs07.redmond.corp.microsoft.com\e$\Deploy\Task.Powershell.Cleanup\Cleanup.Process.ps1"
+	#Set-ExecutionPolicy Bypass "Cleanup.Process.ps1"
+	#Unblock-File "\\Co1bpdcsvctfs07.redmond.corp.microsoft.com\e$\Deploy\Task.Powershell.Cleanup\Cleanup.Process.ps1"
+	Stop-Process -processname "Chrome*" 
+	Stop-Process -processname "IEDriver*"
+    Stop-Process -processname "iexplore*"
+	Stop-Process -processname "PhantomJS*"
+	#Stop-Process -processname "Internet Explorer" 
+} catch {
+
+} finally {
+	
+}
